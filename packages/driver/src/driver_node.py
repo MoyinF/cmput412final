d2c9d1f37b29963264a4aa81e26ee4b1d34701e4
MoyinF <famoyinoluwa@gmail.com>
2023-04-04 20:00:14 -0600

    Stage 2 and 3 code added, along with helper functions. Needs more work.

--- packages/driver/src/driver_node.py
+++ packages/driver/src/driver_node.py
@@ -26,6 +26,7 @@ class DriverNode(DTROS):
             node_name=node_name, node_type=NodeType.GENERIC)
         self.node_name = node_name
         self.veh = rospy.get_param("~veh")
+        self.stall = None # TODO: Get stall number as launch parameter
 
         # Subscribers
         self.sub_camera = rospy.Subscriber("/" + self.veh + "/camera_node/image/compressed", CompressedImage, self.img_callback, queue_size=1, buff_size="20MB")
@@ -132,6 +133,9 @@ class DriverNode(DTROS):
         # Shutdown hook
         rospy.on_shutdown(self.hook)
 
+    def img_callback(self, msg):
+        self.image_msg = msg
+
     def run_(self):
         rate = rospy.Rate(8)  # 8hz
         i = 0
@@ -161,7 +165,7 @@ class DriverNode(DTROS):
         while not self.intersection_detected: # TODO: implement intersection (red line) detection
             if self.image_msg is not None:
                 self.detect_lane(self.image_msg)
-                # self.detect_intersection(self.image_msg)
+                # self.detect_intersection(self.image_msg) # TODO: replace with red line detections
             self.drive()
             rate.sleep()
 
@@ -177,27 +181,34 @@ class DriverNode(DTROS):
                 self.at_detected = self.detect_apriltag(self.image_msg)
             self.drive()
             rate.sleep()
+        # TODO: how are we currently handling the derivative kick problem?
+
+    def correct(self):
+        # TODO: currently when the bot stops it stops at an angle that prevents it from seeing
+        # the yellow tape. I was thinking that if we add code for it to "correct" itself
+        # i.e. re-orient itself so it sees the yellow lines properly
+        # we won't have the problem of it driving off the road when it stops detecting the yellow
+        # we could also add a flag for this within the drive function,
+        # i.e. if we can't see yellow, stop, correct, then keep driving.
+        return
 
     def route1(self):
         # new intersection behaviour defined
         # After crossing red line...
         self.right_turn()
         self.sprint()
-        if self.closest_at == 50:
-            self.drive_to_intersection()
-            rospy.loginfo("Turning left.")
-            self.left_turn()
-        else:
+        if self.closest_at != 50:
             rospy.loginfo("WARNING: Detecting wrong apriltag for route 1.")
-            rospy.loginfo("Turning left.")
-            self.left_turn()
+        self.drive_to_intersection()
+        rospy.loginfo("Turning left.")
+        self.left_turn()
 
         self.sprint()
         if self.closest_at == 56:
             rospy.loginfo("YAY: Almost done with Stage 1. :)")
-            self.drive_to_intersection()
         else:
             rospy.loginfo("WARNING: Detecting wrong apriltag for route 1.")
+        self.drive_to_intersection()
         self.pub_straight()
 
     def route2(self):
@@ -206,32 +217,22 @@ class DriverNode(DTROS):
         self.pub_straight() # TODO: needs fixing, (check function's comments)
         rate = rospy.Rate(4)  # 8hz
         self.sprint()
-        if self.closest_at == 48:
-            rospy.loginfo("Turning right.")
-            self.right_turn()
-        else:
+        if self.closest_at != 48:
             rospy.loginfo("WARNING: Detecting wrong apriltag for route 2.")
-            rospy.loginfo("Turning right.")
-            self.right_turn()
+        self.drive_to_intersection()
+        rospy.loginfo("Turning right.")
+        self.right_turn()
 
         self.sprint()
-        if self.closest_at == 50:
-            self.drive_to_intersection()
-            rospy.loginfo("Turning left.")
-            self.left_turn()
-        else:
+        if self.closest_at != 50:
             rospy.loginfo("WARNING: Detecting wrong apriltag for route 1.")
-            rospy.loginfo("Turning left.")
-            self.left_turn()
+        self.drive_to_intersection()
+        rospy.loginfo("Turning left.")
+        self.left_turn()
 
     def stage1(self):
         rate = rospy.Rate(4)  # 8hz
-        while not self.at_detected: # TODO: adjust distance for detecting apriltag
-            if self.image_msg is not None:
-                self.detect_lane(self.image_msg)
-                self.at_detected = self.detect_apriltag(self.image_msg)
-            self.drive()
-            rate.sleep()
+        self.sprint()
 
         if self.closest_at == 48:
             self.route1()
@@ -242,13 +243,86 @@ class DriverNode(DTROS):
             rospy.loginfo("REPEATING stage 1.") # TODO: Should we do this?
             self.stage1()
 
+    def drive_to_blue_line(self):
+        # like drive_to_intersection but stop farther away to avoid duck murder
+        rate = rospy.Rate(4)
+        self.close_to_blue = False
+        while not self.close_to_blue:
+            if self.image_msg is not None:
+                self.detect_lane(self.image_msg)
+                # self.detect_intersection(self.image_msg) # TODO: replace with blue line detections
+            self.drive()
+            rate.sleep()
+
+        self.stop()
+
+    def check_for_ducks(self):
+        # TODO: I (Mo) intend for us to use both yellow mask and tof sensing for this (if necessary)
+        return False
+
+    def check_for_bot(self):
+        rate = rospy.Rate(4)
+        self.close_to_blue = False
+        while not self.close_to_blue:
+            if self.image_msg is not None:
+                self.detect_lane(self.image_msg)
+                # self.detect_intersection(self.image_msg) # TODO: replace with blue contour/bot detections
+            self.drive()
+            rate.sleep()
+        # TODO: stop at a suitable distance so we have time to switch lanes
+        self.stop()
+
+    def switch_lanes(self):
+        # TODO: Probably switch to English driver. Keep moving till (?). Then switch back.
+        pass
+
+    def avoid_ducks(self):
+        self.sprint()
+        if self.closest_at != 163:
+            rospy.loginfo("WARNING: Detecting wrong apriltag for stage 2.")
+        self.drive_to_blue_line()
+        self.stop() # just to make sure
+        ducks_present = self.check_for_ducks()
+        if ducks_present:
+            while self.check_for_ducks():
+                self.pass_time(1)
+            self.pass_time(5)
+        else:
+            self.pass_time(5)
+
     def stage2(self):
-        self.drive()
-    def stage3(self):
+        self.avoid_ducks()
+        self.check_for_bot()
+        self.switch_lanes()
+        self.avoid_ducks()
         self.drive()
 
-    def img_callback(self, msg):
-        self.image_msg = msg
+    def park(self, stall):
+        # TODO
+        if stall == 1:
+            continue
+        elif stall == 2:
+            continue
+        elif stall == 3:
+            continue
+        elif stall == 4:
+            continue
+        else:
+            rospy.loginfo("WARNING: Wrong input for parking stall number.")
+            rospy.loginfo("Automatically backing into stall 3.")
+
+    def stage3(self):
+        self.sprint()
+        if self.closest_at != 38:
+            rospy.loginfo("WARNING: Detecting wrong apriltag for stage 3.")
+        rospy.loginfo("PARKING.")
+        self.drive_to_intersection()
+        self.pub_straight()
+        while self.at_distance > 0.4:
+            continue
+        if self.closest_at != 227:
+            rospy.loginfo("WARNING: Detecting wrong apriltag for stage 3.")
+        self.park(self.stall)
 
     def detect_lane(self, msg):
         # decodes the image for lane following
@@ -311,6 +385,7 @@ class DriverNode(DTROS):
         self.vel_pub.publish(self.twist)
 
     def intersection_sequence(self):
+        # outdated
         if DEBUG:
             rospy.loginfo("INTERSECTION DETECTED at {}".format(str(self.at_distance)))
 
